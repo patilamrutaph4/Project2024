5e10d4f1abc972653eb73ff110c4e1c8939fc494
patilamrutaph4 <patil.amruta.ph4@gmail.com>
2024-02-22 21:35:30 +0530

    2nd commit

---
+++ eclipse-Amruta-workspace/TestGitHub/src/TestKnowledge/CollectionExamples.java
@@ -0,0 +1,24 @@
+package TestKnowledge;
+import java.util.*;
+
+public class CollectionExamples {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String>list=new ArrayList<String>();
+		list.add(null);
+		list.add(null);
+		list.add("Amruta");
+		list.add("Patil");
+		list.add("Amruta");
+		list.add("Gaikwad");
+		list.add("Mugdha");
+		list.add("Gaikwad");
+		
+		System.out.println("ArrayList is " + list);
+		
+		
+		
+	}
+
+}
